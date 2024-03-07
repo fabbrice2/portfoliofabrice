9044beecab6ff41048ed7b8031bd39471580eb75
fabbrice2 <fabriceamenouglo@gmail.com>
2024-03-07 20:08:24 +0100

    Portfolio update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import FabricEcom from "./pages/FabricEcom";
+import Consisto from "./pages/Consisto";
+import Netflix from "./pages/Netflix";
+import Checklist from "./pages/Checklist";
+import Foodwagon from "./pages/Foodwagon";
+import Spotlight from "./pages/Spotlight";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home /> } />
+      <Route path="/ecommerce" element={<FabricEcom />} />
+      <Route path="/consisto" element={<Consisto />} />
+      <Route path="/netflix" element={<Netflix />} />
+      <Route path="/checklist" element={<Checklist />} />
+      <Route path="/foodwagon" element={<Foodwagon />} />
+      <Route path="/spotlight" element={<Spotlight />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
-}
+};
 
 export default App;
